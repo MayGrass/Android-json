4c79acbc541713b2534d2192fef0d6acf03f90a0
DCH <dching0121@gmail.com>
2019-11-26 10:06:35 +0800

    request產生物件丟進queue

--- app/src/main/java/tw/org/iii/android_json/MainActivity.java
+++ app/src/main/java/tw/org/iii/android_json/MainActivity.java
@@ -3,12 +3,77 @@ package tw.org.iii.android_json;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private MyAdapter myAdapter;
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        queue = Volley.newRequestQueue(this);
+        listView = findViewById(R.id.listView);
+        initListView();
+        fetchRemoteData();
+    }
+
+    private void fetchRemoteData() {
+        StringRequest request = new StringRequest(Request.Method.GET,
+                "http://data.coa.gov.tw/Service/OpenData/ODwsv/ODwsvTravelFood.aspx",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+        queue.add(request);
+    }
+
+    private void initListView() {
+        myAdapter = new MyAdapter();
+        listView.setAdapter(myAdapter);
+    }
+
+    private class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
     }
 }
