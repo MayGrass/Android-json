52bc0dc08ea4d3b4bcdaa9fbaaf4ce7d3ced2e75
DCH <dching0121@gmail.com>
2019-11-26 13:27:06 +0800

    加入要抓的欄位

--- app/src/main/java/tw/org/iii/android_json/DetailActivity.java
+++ app/src/main/java/tw/org/iii/android_json/DetailActivity.java
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.toolbox.StringRequest;
 
 import java.util.HashMap;
 
 public class DetailActivity extends AppCompatActivity {
+    private String id, name, addr, tel, hostwords, feature, latIng, picurl, heart;
+    private ImageView imageView;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,18 @@ public class DetailActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         HashMap<String,String> row = (HashMap<String, String>)(intent.getSerializableExtra("data"));
-        Log.v("DCH", row.get("Name"));
+        //Log.v("DCH", row.get("Name"));
+
+        id = row.get("ID");
+        name = row.get("Name");
+        addr = row.get("Address");
+        tel = row.get("Tel");
+        hostwords = row.get("HostWords");
+        feature = row.get("FoodFeature");
+        latIng = row.get("Coordinate");
+        picurl = row.get("PicURL");
+
+        imageView = findViewById(R.id.detail_img);
+        textView = findViewById(R.id.detail_Content);
     }
 }
